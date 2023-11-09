ca300b837083eea3ef5cc6e551a39ee28fd28320
moazizaa <90508507+moazizaa@users.noreply.github.com>
2023-11-09 03:01:28 +0200

    Add files via upload
    Signed-off-by: moazizaa <90508507+moazizaa@users.noreply.github.com>

--- AnonXMusic/plugins/play/bot.py
+++ AnonXMusic/plugins/play/bot.py
@@ -1,4 +1,4 @@
-
+#✘ CAESAR MUSIC @c_a_s_e_r_h ✘
 import asyncio
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton 
 from pyrogram import filters, Client
@@ -17,21 +17,21 @@ async def Italymusic(client: Client, message: Message):
     chat_id = message.chat.id
     try:
         member = await client.get_chat_member(chat_id, user_id)
-        if user_id == 1183747742:
-             rank = "⋆ المبرمج مصطفي العزايري..🙂♥️"
+        if user_id == 6300938349:
+             rank = "**هقر السورس🥹❤️**"
         elif user_id == OWNER_ID:
-             rank = "⋆ صاحب البوت ومطوري حبيبي..🙂♥️"
+             rank = "صاحب البوت يمعلم ⇇ اهلا مطوري الغالي كلو تحت السيطره يمعلم🦋🥹"
         elif member.status == 'creator':
-             rank = "⋆ صاحب الجروب حبيب الكل..🙂♥️"
+             rank = "**وه مالك الجروب⇇ يمعلم دخولك رايق سبب حرايق🥹🦋**"
         elif member.status == 'administrator':
-             rank = "⋆ مشرف الجروب الغالي..🙂♥️"
+             rank = "**مشرف الجروب⇇ ينهار ابيض كابيه شاي للمعلم هنا يبني 🥹❤️**"
         else:
-             rank = "لاسف انت عضو فقير🥺💔"
+             rank = "**لاسف انت عضو فقير🥺💔**"
     except Exception as e:
         print(e)
         rank = "مش عرفنلو مله ده😒"
     async for photo in client.get_chat_photos("me", limit=1):
+                    await message.reply_photo(photo.file_id,       caption=f"""**ـــــــ⌘ نعم يمعلم انا بتاع الدي جي ⌘ـــــ اتفضل ياا ⇇:** {italy} عوز اي🤍🦋\n**وانا بوت يمعلم واسمي ⇇ :** {bot_name} 🥺🍓\n**رتبتك يمعلم ⇇ :** {rank}""", reply_markup=keyboard)
 
-                    await message.reply_photo(photo.file_id,       caption=f"""⋆ اسمي الكيوت ⇇: {bot_name} 🥹♥️""", reply_markup=keyboard)
-
+#✘ CAESAR MUSIC @c_a_s_e_r_h ✘
 
